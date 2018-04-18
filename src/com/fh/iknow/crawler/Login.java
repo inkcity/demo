0dbf2ee14f1a2843eb72578d0dd4ed060039d1d6
inkcity <920907676@qq.com>
2018-04-18 18:12:01 +0800

    登录模块，不返回视图，只返回json数据（使用fastjson）

--- src/com/fh/iknow/crawler/Login.java
+++ src/com/fh/iknow/crawler/Login.java
@@ -6,22 +6,45 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
 
 @Controller
 public class Login {
 
+//	@RequestMapping(value="/login", method=RequestMethod.POST)
+//	public String login(@RequestParam("username") String username
+//			, @RequestParam("password") String password
+//			, HttpSession session) {
+//		System.out.println("enter login");
+//		System.out.println(username);
+//		System.out.println(password);
+//		if ("920907676@qq.com".equals(username) && "aaa".equals(password)) {
+//			session.setAttribute("username", username);
+//			return "login";
+//		} else {
+//			return "loginFail";
+//		}
+//	}
+	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
+	@ResponseBody
 	public String login(@RequestParam("username") String username
 			, @RequestParam("password") String password
 			, HttpSession session) {
 		System.out.println("enter login");
 		System.out.println(username);
 		System.out.println(password);
+		JSONObject result = new JSONObject();
 		if ("920907676@qq.com".equals(username) && "aaa".equals(password)) {
 			session.setAttribute("username", username);
-			return "login";
+			result.put("msg", "true");
+			System.out.println(result.toJSONString());
+			return result.toJSONString();
 		} else {
-			return "loginFail";
+			result.put("msg", "false");
+			return result.toJSONString();
 		}
 	}
 }
